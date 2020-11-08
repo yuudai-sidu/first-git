a1ee6c0b7a96ec2bc713b167a97245f228772e8d
yuudai tanaka <sy.markx130@gmail.com>
2020-11-08 10:42:08 +0000

    CRUD tasks

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,50 @@
+class TasksController < ApplicationController
+    
+    def index
+        @tasks = Task.all
+  end
+
+  def show
+       @task = Task.find(params[:id])
+
+  def new
+      @task = Task.new
+  end
+
+  def create
+      @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'タスクが投稿されました'
+      redirect_to @task
+    else
+      flash[:danger] = 'タスクが投稿されません'
+      render :new
+    end
+  end
+
+ 
+ 
+  def edit
+      @task = Task.find(params[:id])
+  end
+
+  def update
+     @task = Task.find(params[:id])
+
+  if @task.update(task_params)
+    flash[:success] = 'タスクが編集されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = 'タスクが編集されませんでした'
+    render :new
+  end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'タスクが削除されました'
+    redirect_to tasks_url
+  end
