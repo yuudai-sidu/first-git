c198ce3790b4349b0004fc1eef92b8cfcada1ddb
yuudai tanaka <sy.markx130@gmail.com>
2020-11-10 09:47:27 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -60,10 +60,7 @@ def set_task
     @task = Task.find(params[:id])
   end
 
-def task_params
-  params.require(:task).permit(:content)
-  
-end
+
 def task_params
     params.require(:task).permit(:content, :status)
   end
